38a414cd655773c03a5d75c0a197cd9ac151969b
Jeff Griffiths <jeff@canuckistani.ca>
2011-02-21 13:45:19 -0800

    added deps script, gitignore

---
+++ scripts/php-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pear channel-discover pear.pdepend.org 
+pear channel-discover pear.phpmd.org 
+pear channel-discover pear.phpunit.de
+pear channel-discover components.ez.no
+pear channel-discover pear.symfony-project.com
+
+pear install pdepend/PHP_Depend-beta
+pear install phpmd/PHP_PMD-alpha
+pear install phpunit/phpcpd
+pear install PHPDocumentor
+pear install PHP_CodeSniffer
+pear install --alldeps phpunit/PHP_CodeBrowser-alpha
+pear install --alldeps phpunit/PHPUnit
+
